fb5c4b72cead2e391bed9fbb7e6d2d8849808848
Aiverson <johnaiversonabong@gmail.com>
2025-10-08 23:08:42 +0800

    Update files

--- js/footer.js
+++ js/footer.js
@@ -3,10 +3,10 @@ footer.innerHTML = `
     <div class="footer-content">
         <p>&copy; 2025 John Aiverson Abong. All Rights Reserved.</p>
         <div class="socials">
-            <a href="#"><i class='bx bxl-facebook'></i></a>
-            <a href="#"><i class='bx bxl-instagram'></i></a>
-            <a href="#"><i class='bx bxl-github'></i></a>
-            <a href="#"><i class='bx bxl-linkedin'></i></a>
+            <a href="https://www.facebook.com/aiversonpalomar"><i class='bx bxl-facebook'></i></a>
+            <a href="https://www.instagram.com/aiversonpalomar"><i class='bx bxl-instagram'></i></a>
+            <a href="https://github.com/aiverson11"><i class='bx bxl-github'></i></a>
+            <a href="https://www.linkedin.com/in/johnaiversonabong/"><i class='bx bxl-linkedin'></i></a>
         </div>
     </div>
 `;
